e2bb34e4c409992e3176f7d89d269e4fd9cf41eb
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-05 20:30:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,9 +37,12 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.series_id) FROM characters 
+  "SELECT characters.name, COUNT(books.id) FROM characters 
     JOIN series 
       ON series.id = characters.series_id 
       GROUP BY series.id "
+    JOIN books 
+      ON books.series_id = series.id 
+      ORDER BY COUNT(books.id) DESC; "
       
 end
