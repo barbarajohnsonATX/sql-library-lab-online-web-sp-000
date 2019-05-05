39300da30ed6b809423be157f66062a9cfb45b35
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-05 20:30:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,7 +40,7 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.id) FROM characters 
     JOIN series 
       ON series.id = characters.series_id 
-      GROUP BY series.id "
+      GROUP BY series.id 
     JOIN books 
       ON books.series_id = series.id 
       ORDER BY COUNT(books.id) DESC; "
