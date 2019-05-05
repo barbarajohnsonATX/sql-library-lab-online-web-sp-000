636725582f635086123e48e1a4e48f67835902b7
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-05 20:01:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT Authors.name, "
+  "SELECT Authors.name, Series.subgenres"
 end
 
 def select_series_title_with_most_human_characters
