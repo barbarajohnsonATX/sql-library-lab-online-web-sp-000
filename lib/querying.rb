5e6ce08d2c41ec7aeb231c1048b58dcdd1038de9
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-05 20:15:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_series_title_with_most_human_characters
   "SELECT Series.title FROM Series 
     JOIN Characters 
       ON Series.id = Characters.series_id
-      GROUP BY Series.title 
+      GROUP BY Series.id
       HAVING Characters.species = 'human'
     ORDER BY COUNT(Characters.species) DESC 
     LIMIT 1;"
