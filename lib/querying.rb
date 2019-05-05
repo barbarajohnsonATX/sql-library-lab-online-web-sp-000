f76599e9d38e78647b7129fd992d069be29a72d9
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-05 19:52:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT Characters.species, SUM(Characters.species) FROM Characters 
-    ORDER BY SUM(Characters.species) DESC 
+    ORDER BY SUM(Characters.species) 
     LIMIT 1; "
 end
 
